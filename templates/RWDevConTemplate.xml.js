458acafe0c1ad4f7f778ade4ae6ecf5db90d349e
Christian Siek <christiansiek@Christians-MacBook-Air.local>
2015-11-13 12:58:25 +0100

    test2

--- templates/RWDevConTemplate.xml.js
+++ templates/RWDevConTemplate.xml.js
@@ -5,7 +5,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
       
       .main {
 
-      	background-color: #4A148C;
+      	background-color: #FFEBE;
 
       }
        
